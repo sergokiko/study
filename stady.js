9ff6502e57b82eeded97d2d0adba9ba4eb3af887
sergokiko <conbats8066>
2020-07-06 14:16:32 +0300

    sergojf

--- stady.js
+++ stady.js
@@ -19,3 +19,19 @@ console.log('hello')
 console.log('hello')
 console.log('hello')
 console.log('hello')
+const  delay = (wait = 1000) => {
+ const promise = new Promise((resolve,reject) => {
+  setTimeout(() =>{
+   resolve()
+  },wait)
+ })
+ return promise
+}
+
+delay (2500)
+    .then(() =>{
+     console.log('After 2 second')
+    })
+    .catch(err => console.error((err)))
+    .finally(() =>console.log(('Finally')))
+urddrt
